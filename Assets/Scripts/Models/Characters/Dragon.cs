cb75e489c554cafde338f56a19cce006754faff9
QuakyCZ <kvaky.kratschmer@gmail.com>
2020-03-25 11:52:58 +0100

    dsdfs

---
+++ Assets/Scripts/Models/Characters/Dragon.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace Models.Characters
+{
+    
+    public class Dragon : Enemy
+    {
+        protected override void Attack()
+        {
+            charged = false;
+            ResetCoolDown();
+            DoFireAttack();
+        }
+
+        private void DoFireAttack()
+        {
+            Debug.Log( "Enemy attacks" );
+            animator.SetBool( "isAttacking", true );
+        }
+        
+    }
+}
